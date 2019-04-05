d8480bbae5e1ec2796d6ea78f453c495d3dd5c5a
zwPapEr <zw.paper@gmail.com>
2019-04-05 14:53:55 +0800

    * :art: move white space to one place, delete useless

--- config/init-languages.el
+++ config/init-languages.el
@@ -1,5 +1,5 @@
 ;;; package --- Summary
-;; C++, C, Go, Python
+;; C++, C, Go, Python, Rust, Haskell
 ;;; Commentary:
 
 ;;; Code:
@@ -80,44 +80,22 @@
 ;   (setq-default c-basic-offset 4)
 ;   (setq flycheck-clang-language-standard "gnu99"))
 
-;;; Go
-; (setq gofmt-command "goimports")
-; (defun my-go-mode-hook ()
-;   "Auto format and import on save."
-; ;  (add-hook 'before-save-hook 'gofmt-before-save)
-;   (add-hook 'before-save-hook 'lsp-format-buffer)
-;   (if (not (string-match "go" compile-command))
-;       (set (make-local-variable 'compile-command)
-;            "go generate && go build -v && go test -v && go vet"))
-;   (local-set-key (kbd "M-.") 'godef-jump))
-;
-;(add-hook 'go-mode-hook #')
-; (add-to-list 'load-path "~/.emacs.d/elpa/go-mode-20160715.1705")
-; (add-to-list 'load-path "~/.emacs.d/elpa/auto-complete-20150618.1949/")
-; (add-to-list 'load-path "~/.emacs.d/elpa/popup-20150626.711/")
-; (require 'go-mode-load)
-; (require 'go-autocomplete)
-; (require 'auto-complete-config)
-; (add-hook 'go-mode-hook 'go-eldoc-setup)
-; (require 'golint)
+;; Go
 (use-package go-mode
   :init
   :config
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
-           "go generate && go build -v && go test -v && go vet"))
-  :hook
-  (yas-minor-mode))
+           "go generate && go build -v && go test -v && go vet")))
 
 ;;; Rust
 (use-package rust-mode
   :init
   :config
   (with-eval-after-load 'lsp-mode
-    (setq lsp-rust-rls-command '("rustup" "run" "nightly" "rls")))
-  :hook
-  (rust-mode . flycheck-mode)
-  (rust-mode . lsp))
+    (setq lsp-rust-rls-command '("rustup" "run" "nightly" "rls"))))
+;  :hook
+;  (rust-mode . flycheck-mode))
 
 ;; ;; python
 ;(use-package lsp-python
@@ -137,22 +115,18 @@
   :init
   :config)
 
+(use-package haskell-mode
+  :mode ("\\.hs\\'" . haskell-mode)
+  :init
+  (add-hook 'haskell-mode-hook
+            (lambda ()
+              (setq compile-command "stack build --fast --test --bench --no-run-tests --no-run-benchmarks"))))
 
-;;   (setq   python-shell-interpreter "ipython"
-;;           python-shell-interpreter-args "--colors=Linux --profile=default --simple-prompt -i"
-;;           python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-;;           python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-;;           python-shell-completion-setup-code
-;;           "from IPython.core.completerlib import module_completion"
-;;           python-shell-completion-module-string-code
-;;           "';'.join(module_completion('''%s'''))\n"
-;;           python-shell-completion-string-code
-;;           "';'.join(get_ipython().Completer.all_completions('''%s'''))\n"))
-;;
-;; (require 'ansi-color)
-;; (define-coding-system-alias 'UTF-8 'utf-8)
-;; (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
-;; (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on t)
+(use-package docker
+  :commands docker-mode)
+
+(use-package dockerfile-mode
+  :mode "Dockerfile\\'")
 
 ;; Markdown
 (use-package markdown-mode
