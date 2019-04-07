d7c993e2a84c7297a85a138be5c90ece037f8d58
zwPapEr <zw.paper@gmail.com>
2019-04-07 10:31:01 +0800

    org: :art: using # for place, @ for people

--- config/init-org.el
+++ config/init-org.el
@@ -7,6 +7,7 @@
 
 ;;; org mode
 (use-package org
+  :ensure org-plus-contrib
   :bind (("C-M-f" . org-do-demote)
          ("C-M-b" . org-do-promote)
          ("C-M-p" . org-move-subtree-up)
@@ -16,25 +17,28 @@
   (setq org-agenda-files (list "~/Dropbox/org-mode/tasks/"
                                "~/Dropbox/org-mode/tasks/work.org"
                                "~/Dropbox/org-mode/tasks/family.org"))
+  ;; Custom commands
   (setq org-agenda-custom-commands
-      '(("o" "At the office" tags-todo "@Office"
-         ((org-agenda-overriding-header "Office")))
-        ("p" "At the home" tags-todo "@Home"
-         ((org-agenda-overriding-header "Home")))
-        ("r" . "Review")
-        ("ry" "Closed Yesterday"
-         tags (concat "+TODO=\"DONE\""
-                      "+CLOSED>=\""
-                      (format-time-string "[%Y-%m-%d]" (time-subtract (current-time) (days-to-time 1)))
-                      "\""))
-        ("rw" "Get Current: Review Previous Week"
-         ((agenda "" ((org-agenda-start-day (concat "-" (number-to-string (+ 13 (nth 6 (decode-time)))) "d"))
-                      (org-agenda-span (+ 14 (nth 6 (decode-time))))
-                      (org-agenda-repeating-timestamp-show-all t)
-                      (org-agenda-entry-types '(:deadline :timestamp :sexp)) ; show due tasks, meetings
-                      (org-agenda-show-log t)
-                      (org-agenda-prefix-format "%-12t% s"))))
-         ))))
+        '(("o" "At the office" tags-todo "#office"
+           ((org-agenda-overriding-header "#office")))
+          ("h" "At the home" tags-todo "#home"
+           ((org-agenda-overriding-header "home")))
+          ("p" "Priority" tags-todo "+PRIORITY=\"A\"")
+          ("w" "Waitting" todo "WAITING")
+          ("r" . "Review")
+          ("ry" "Closed Yesterday"
+           tags (concat "+TODO=\"DONE\""
+                        "+CLOSED>=\""
+                        (format-time-string "[%Y-%m-%d]" (time-subtract (current-time) (days-to-time 1)))
+                        "\""))
+          ("rw" "Get Current: Review Previous Week"
+           ((agenda "" ((org-agenda-start-day (concat "-" (number-to-string (+ 13 (nth 6 (decode-time)))) "d"))
+                        (org-agenda-span (+ 14 (nth 6 (decode-time))))
+                        (org-agenda-repeating-timestamp-show-all t)
+                        (org-agenda-entry-types '(:deadline :timestamp :sexp)) ; show due tasks, meetings
+                        (org-agenda-show-log t)
+                        (org-agenda-prefix-format "%-12t% s"))))
+           ))))
 
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
@@ -113,7 +117,12 @@
 (setq org-todo-keyword-faces
       '(("NEXT" . (:foreground "red" :weight bold))
         ("WAITING" . "magenta")))
-(setq org-tag-alist '(("@office" . ?o) ("@home" . ?h) ("@computer" . ?c)))
+(setq org-tag-alist (quote (("#errand" . ?e)
+                            ("#office" . ?o)
+                            ("#home" . ?h)
+                            ("#computer" . ?c)
+                            (:newline)
+                            ("HOLD" . ?H))))
 (setq org-log-done 'time)
 (setq org-log-into-drawer t)
 (setq org-log-state-notes-insert-after-drawers nil)
