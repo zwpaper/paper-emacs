8d3f9c18407fd54002776e45972d7d022b624e04
zwPapEr <zw.paper@gmail.com>
2019-06-15 10:49:58 +0800

    markup: :sparkles: json graphQL mode

---
+++ config/init-markup.el
@@ -0,0 +1,32 @@
+;; init-markup.el --- Initialize Markup Language configurations.	-*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 zwPapEr
+
+;; Author: zwPapEr <zw.paper@gmail.com>
+;; URL: https://github.com/zwpaper/.emacs.d
+
+;; This file is not part of GNU Emacs.
+;;
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;;
+
+;;; Commentary:
+;;
+;; Markup configurations.
+;;
+
+;;; Code:
+
+(use-package graphql-mode)
+
+(use-package json-mode)
+
+(provide 'init-markup)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-markup.el ends here
