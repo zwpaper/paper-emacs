a91c834ffc9bf4fcc2535914e385246009e3cf5d
zwPapEr <zw.paper@gmail.com>
2019-08-18 15:59:17 +0800

    js: :sparkles: using lsp-js for js mdoe

---
+++ config/init-typescript.el
@@ -0,0 +1,15 @@
+;;; package --- Summary
+;; TypeScript init
+;;; Commentary:
+
+;;; Code:
+
+(eval-when-compile
+  (require 'init-const))
+
+;;; Using built-in support in lsp-mode
+;;; yarn global add typescript-language-server
+;;; yarn global add typescript
+
+(provide 'init-typescript)
+;;; init-typescript.el ends here
